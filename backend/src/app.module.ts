48ee8f487b6702135457df210bf7ce532f7f23c1
“OmsinghBais“ <baisos@rknec.edu>
2025-12-26 15:55:14 +0530

    Frontend ready for Vercel

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SprintModule } from './sprint/sprint.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'omsinghbais',
-      password: '15O04m06@',
-      database: 'scrummate',
-      autoLoadEntities: true,
-      synchronize: true, // dev only
       url: process.env.DATABASE_URL,
- 
-  
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
-    SprintModule,
   ],
 })
 export class AppModule {}
